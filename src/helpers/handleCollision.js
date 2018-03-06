7debd4e9c241a72fd7fe5ab68d737d0eaa1d9667
Shubham Mathur <shubham.mathur@engineer.com>
2018-03-06 13:18:34 +0530

    Collision test case added

--- src/helpers/handleCollision.js
+++ src/helpers/handleCollision.js
@@ -1,14 +1,14 @@
 const Models = require('../../models');
-const { longToShort } = require('../helpers/longToShort');
+const shortifyObj = require('../helpers/longToShort');
 
 function handleCollision(longUrl, initial, final) {
-  const shortUrl = longToShort(longUrl, initial, final);
+  const shortUrl = shortifyObj.longToShort(longUrl, initial, final);
   return Models.urls.findOne({
     where: {
       shortUrl,
     },
   }).then((shortUrlDb) => {
-    if (shortUrlDb !== shortUrl) return shortUrl;
+    if (shortUrlDb === null || shortUrlDb.shortUrl !== shortUrl) return shortUrl;
     else if (final < 31) { return handleCollision(longUrl, initial + 1, final + 1); }
     return null;
   });
--- test/handlers/handleLongToShort.test.js
+++ test/handlers/handleLongToShort.test.js
@@ -1,8 +1,9 @@
 const { handleLongToShort } = require('../../src/handlers/handleLongToShort');
+const Sinon = require('sinon');
+const shortifyObj = require('../../src/helpers/longToShort');
 const Models = require('../../models');
 
-beforeAll(done => Models.urls.destroy({ truncate: true }).then(done()));
-afterAll(done => Models.urls.destroy({ truncate: true }).then(done()));
+beforeEach(done => Models.urls.destroy({ truncate: true }).then(done()));
 
 
 describe('function handleToShort', () => {
@@ -22,6 +23,30 @@ describe('function handleToShort', () => {
             expect(urldata1.shortUrl).toBe(urldata2.shortUrl);
             done();
           });
+      });
+  });
+  it('should recurse and insert if there is a collision in MD5', (done) => {
+    const url1 = 'this is string 1';
+    const url2 = 'this is string 2';
+    const stub = Sinon.stub(shortifyObj, 'longToShort');
+    stub.withArgs(url1, 0, 6).returns('123456');
+    stub.withArgs(url2, 0, 6).returns('123456');
+    stub.withArgs(url2, 1, 7).returns('ghijkl');
+
+    handleLongToShort(url1)
+      .then(() => {
+        handleLongToShort(url2).then((result2) => {
+          expect(result2.shortUrl).toBe('ghijkl');
+          Models.urls.findAll({
+            where: {
+              shortUrl: result2.shortUrl,
+            },
+          }).then((searchResult) => {
+            expect(searchResult.length).toBe(1);
+            stub.restore();
+            done();
+          });
+        });
       });
   });
 });
