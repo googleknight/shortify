03b4d9acf4eb51362308095ec4675f0016c2d9fb
Shubham Mathur <shubham.mathur@engineer.com>
2018-03-07 09:11:01 +0530

    Read route fixed and tested

--- src/routes/shortifyUrl.js
+++ src/routes/shortifyUrl.js
@@ -29,7 +29,7 @@ module.exports = [
       Models.urls.findOne({ where: { shortUrl: request.query.shortUrl } })
         .then((urldata) => {
           response({
-            shortUrl: urldata.longUrl,
+            longUrl: urldata.longUrl,
             statusCode: 200,
           });
         })
--- test/routes/shortifyUrl.test.js
+++ test/routes/shortifyUrl.test.js
@@ -37,8 +37,8 @@ describe('Test POST shortify route', () => {
 
 
 describe('Test GET shortify route', () => {
-  beforeEach(done => Models.urls.create({ longUrl: 'www.somelonglongurl', shortUrl: 'asdasd' }).then(done()));
-  afterEach(done => Models.urls.destroy({ where: { longUrl: 'www.somelonglongurl', shortUrl: 'asdasd' } }).then(done()));
+  beforeEach(done => Models.urls.create({ longUrl: 'www.somelonglongurl.com', shortUrl: 'asdasd' }).then(done()));
+  afterEach(done => Models.urls.destroy({ where: { longUrl: 'www.somelonglongurl.com', shortUrl: 'asdasd' } }).then(done()));
   it('should gives 200 as success code', (done) => {
     supertest(server.listener)
       .get('/shortify?shortUrl=asdasd')
@@ -49,19 +49,13 @@ describe('Test GET shortify route', () => {
       .catch((reason) => { throw reason; });
   });
 
-  // it('should makes an entry in database for a new long url', (done) => {
-  //   supertest(server.listener)
-  //     .post('/shortify')
-  //     .send({
-  //       longUrl: 'www.testinglongUrl.com',
-  //     })
-  //     .then((response) => {
-  //       Models.urls.findOne({ where: { longUrl: 'www.testinglongUrl.com' } })
-  //         .then((urldata) => {
-  //           expect(`www.${urldata.shortUrl}.com`).toBe(response.body.shortUrl);
-  //           done();
-  //         })
-  //         .catch((reason) => { throw reason; });
-  //     });
-  // });
+  it('should reads from database for a short url', (done) => {
+    supertest(server.listener)
+      .get('/shortify?shortUrl=asdasd')
+      .then((response) => {
+        expect(response.body.longUrl).toBe('www.somelonglongurl.com');
+        done();
+      })
+      .catch((reason) => { throw reason; });
+  });
 });
